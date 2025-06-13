1b4ea8afc6976e49b3dc71e1243e248ec8719a92
Andrew Halisky <ajhalisky@gmail.com>
2025-06-13 19:23:00 -0400

    Finished code 6 with trees assignment

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,15 +1,20 @@
+using System.ComponentModel.Design.Serialization;
 using System.Transactions;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client.Interfaces;
 
 public class Node
 {
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
-
+    private Node? _root;
+    
     public Node(int data)
     {
         this.Data = data;
     }
+
     public void Insert(int value)
     {
         // TODO Start Problem 1
@@ -33,18 +38,40 @@ public class Node
         {
             return;
         }
-        return;
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (Data == value)
+        {
+            return true;
+        }
+        if (Left != null && Left.Contains(value))
+        {
+            return true;
+        }
+        else if (Right != null && Right.Contains(value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;     
+        }  
     }
+
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left == null && Right == null)
+        {
+            return 1;
+        }
+        int leftHeight = (Left != null) ? Left.GetHeight(): -1;
+        int rightHeight = (Right != null) ? Right.GetHeight(): -1;
+        // Replace this line with the correct return statement(s)
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
